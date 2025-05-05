9e87c7ebe34faf4a07cb7b1ffc14b67d82be9e73
Filipb02 <filip@batterimannen.com>
2025-05-05 11:41:28 +0200

    Push

--- FYBooking/src/App.jsx
+++ FYBooking/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainProvider from "./providers/MainProvider";
 import StartPage from "./pages/StartPage";
 import MainPage from "./pages/MainPage";
@@ -8,10 +9,26 @@ import UserPage from "./pages/UserPage";
 function App() {
   return (
     <MainProvider>
-      <StartPage />
-      <MainPage />
-      <UserPage />
-      <AdminPage />
+      <BrowserRouter>
+        <Routes>
+          <Route
+          path="/"
+          element={<StartPage/>}
+          />
+          <Route
+          path="/main"
+          element={<MainPage/>}
+          />
+          <Route
+          path="/user"
+          element={<UserPage/>}
+          />
+          <Route
+          path="/admin"
+          element={<AdminPage/>}
+          />
+        </Routes>
+      </BrowserRouter>
     </MainProvider>
   );
 }
