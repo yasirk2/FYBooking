b550b1ceb0e721047a1f1e93de13f92ab0aba622
Filipb02 <filip@batterimannen.com>
2025-05-07 15:50:17 +0200

    Create New Users

--- FYBooking/src/components/UsersAdmin.jsx
+++ FYBooking/src/components/UsersAdmin.jsx
@@ -1,10 +1,43 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MainContext from "../providers/contexts/MainContext";
-import { addUsers } from "../data/db";
+import { getUsers, addUsers } from "../data/db";
 
 const UsersAdmin = () => {
   const [formVisibility, setFormVisibility] = useState(false);
+  const [newUserData, setNewUserData] = useState({
+    username: "",
+    password: "",
+    role: "client",
+  });
+  const [users, setUsers] = useState(getUsers());
   const { setAdminPageDisplay } = useContext(MainContext);
+
+  // Handles data changes in the form for new users data
+  const handleChange = (e, data) => {
+    const { name, value } = e.target;
+    data((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Function that creates the new user and stores it
+  const creatUser = (e) => {
+    e.preventDefault();
+
+    let tempOrg = "FYBooking";
+
+    const uniqueId = users.length + 1;
+
+    const newUser = {
+      ...newUserData,
+      organization: tempOrg,
+      user_id: uniqueId,
+    };
+
+    addUsers(newUser);
+    setUsers(getUsers());
+  };
 
   const goBack = () => {
     setAdminPageDisplay(null);
@@ -15,13 +48,45 @@ const UsersAdmin = () => {
       {formVisibility === true && (
         <div>
           <button onClick={() => setFormVisibility(false)}>X</button>
-          <form className="add-user-form" onSubmit={console.log("hej")}>
-            <label htmlFor="username">Username</label>
-            <input type="text"  name="username" id="username" placeholder="Username"/>
+          <form className="add-user-form" onSubmit={creatUser}>
+            <label htmlFor="new-user-username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="new-user-username"
+              onChange={(e) => handleChange(e, setNewUserData)}
+              placeholder="Username"
+            />
             <label htmlFor="new-user-password">Password</label>
-            <input type="password" name="new-user-password" id="new-user-password" placeholder="Password"/>
-            <label htmlFor="role">Role</label>
-            <input type="text" name="role" id="role" placeholder="Role"/>
+            <input
+              type="password"
+              name="password"
+              id="new-user-password"
+              onChange={(e) => handleChange(e, setNewUserData)}
+              placeholder="Password"
+            />
+            <fieldset>
+              <legend>Role</legend>
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="client"
+                  defaultChecked
+                  onChange={(e) => handleChange(e, setNewUserData)}
+                />
+                Client
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="admin"
+                  onChange={(e) => handleChange(e, setNewUserData)}
+                />
+                Admin
+              </label>
+            </fieldset>
             <button type="submit">Create User</button>
           </form>
         </div>
