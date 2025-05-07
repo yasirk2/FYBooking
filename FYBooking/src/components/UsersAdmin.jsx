8bdc15650e6071b68ffa4c479c7727de992b5ab9
Filipb02 <filip@batterimannen.com>
2025-05-07 14:24:30 +0200

    Form and Navigation Memory

--- FYBooking/src/components/UsersAdmin.jsx
+++ FYBooking/src/components/UsersAdmin.jsx
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import MainContext from "../providers/contexts/MainContext";
+import { addUsers } from "../data/db";
 
 const UsersAdmin = () => {
+  const [formVisibility, setFormVisibility] = useState(false);
   const { setAdminPageDisplay } = useContext(MainContext);
 
   const goBack = () => {
@@ -10,7 +12,23 @@ const UsersAdmin = () => {
 
   return (
     <>
-      <button>Add User</button>
+      {formVisibility === true && (
+        <div>
+          <button onClick={() => setFormVisibility(false)}>X</button>
+          <form className="add-user-form" onSubmit={console.log("hej")}>
+            <label htmlFor="username">Username</label>
+            <input type="text"  name="username" id="username" placeholder="Username"/>
+            <label htmlFor="new-user-password">Password</label>
+            <input type="password" name="new-user-password" id="new-user-password" placeholder="Password"/>
+            <label htmlFor="role">Role</label>
+            <input type="text" name="role" id="role" placeholder="Role"/>
+            <button type="submit">Create User</button>
+          </form>
+        </div>
+      )}
+      <button onClick={() => setFormVisibility(true)}>Add New User</button>
+      <h2>Existing Users</h2>
+      <div></div>
       <button onClick={goBack}>Back</button>
     </>
   );
--- FYBooking/src/providers/MainProvider.jsx
+++ FYBooking/src/providers/MainProvider.jsx
@@ -1,10 +1,20 @@
 import MainContext from "./contexts/MainContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MainProvider = ({ children }) => {
   const [userPageDisplay, setUserPageDisplay] = useState(null);
   const [adminPageDisplay, setAdminPageDisplay] = useState(null);
-  const [previousPage, setPreviousPage] = useState(null);
+  const [previousPage, setPreviousPage] = useState(() => {
+    const storedPage = sessionStorage.getItem("previousPage");
+    return storedPage || null;
+  });
+
+  // Uppdaterar previousPage till den senaste sidan
+  useEffect(() => {
+    if (previousPage) {
+      sessionStorage.setItem("previousPage", previousPage);
+    }
+  }, [previousPage]);
 
   return (
     <MainContext.Provider
